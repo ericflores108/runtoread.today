c270d19c34c34c2cbd75e7cb57583d84c508579d
Eric Flores <95382086+ericflores108@users.noreply.github.com>
2023-09-12 17:20:33 -0700

    add redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: 'about', redirectTo: '', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
